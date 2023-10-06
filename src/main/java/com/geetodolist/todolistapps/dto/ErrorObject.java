5b06e91cb832a8c687f82ff099d6470e9d9806f9
“Agus <agus40994@gmail.com>
2023-10-07 00:35:19 +0700

    Create users and add exception

---
+++ src/main/java/com/geetodolist/todolistapps/dto/ErrorObject.java
@@ -0,0 +1,10 @@
+package com.geetodolist.todolistapps.dto;
+import lombok.Data;
+import java.util.Date;
+
+@Data
+public class ErrorObject {
+    private Integer statusCode;
+    private String message;
+    private Date timeStamp;
+}

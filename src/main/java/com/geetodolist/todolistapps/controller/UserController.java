54e73d7d8a579517abb5f2c9edb61557a50953f5
“Agus <agus40994@gmail.com>
2023-10-10 23:18:02 +0700

    Add Rest for Update user and deactivate

--- src/main/java/com/geetodolist/todolistapps/controller/UserController.java
+++ src/main/java/com/geetodolist/todolistapps/controller/UserController.java
@@ -1,13 +1,14 @@
 package com.geetodolist.todolistapps.controller;
 
+import com.geetodolist.todolistapps.dto.users.RequestUser;
 import com.geetodolist.todolistapps.entity.Users;
 import com.geetodolist.todolistapps.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
@@ -25,4 +26,22 @@ public class UserController {
     public ResponseEntity<Users> findUser(){
         return new ResponseEntity<>(userService.readUserByUserId(), HttpStatus.OK);
     }
+
+    @PutMapping(
+            path = "/user",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<Users> updateUser(@Valid @RequestBody RequestUser user){
+        return new ResponseEntity<>(userService.updateUser(user), HttpStatus.OK);
+    }
+
+    @DeleteMapping(
+            path = "/deactivate"
+    )
+    public ResponseEntity<HttpStatus> deleteUser(){
+        userService.deleteUser();
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
 }
